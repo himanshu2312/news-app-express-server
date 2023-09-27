3735ee4d1ffca9c0949fb36fbaced89ff988d99d
himanshu2312 <himanshu2312gupta@gmail.com>
2023-09-27 15:25:45 +0530

    express server news api

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import userRouter from "./routes/user.js"
-import questionRouter from "./routes/question.js"
-import answerRouter from "./routes/answer.js"
-import dotenv from 'dotenv'
+import express, { response } from "express";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+import cors from "cors"
 
-const app = express();
-dotenv.config()
+const app=express();
+dotenv.config();
 
 app.use(express.json({limit: "30mb",extended: true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
 app.use(cors())
 
 app.get('/',(req,res)=>{
-      res.send("Stack Overflow API Running")
+      res.send("News API Running")
 })
 
-app.use('/user',userRouter)
-app.use('/questions',questionRouter)
-app.use('/answers',answerRouter)
+app.post('/news', async(req,res)=>{
+      const { category, country,pageSize,page } = req.body;
+      console.log( category,country,pageSize,page);
+      const api_key = process.env.API_KEY;
+      console.log(api_key)
+      try {
+          let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${api_key}&page=${page}&PageSize=${pageSize}`;
+          let data = await fetch(url);
+          let parseData = await data.json();
+          console.log(parseData)
+          res.status(200).json(parseData);
+        } catch (e) {
+          console.log(e);
+          res.status(404).json({ message: "Something went wrong" });
+        }
+})
 
-const PORT = process.env.PORT || 5000
-const DATABASE_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 4000
 
-mongoose.connect( DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=> app.listen(PORT, ()=> console.log(`server running on ${PORT}`)))
-.catch(e => console.log(e))
-
- 
+try{
+      app.listen(PORT, ()=> console.log(`server running on ${PORT}`))
+}
+catch(e){
+      console.log(e)
+}
